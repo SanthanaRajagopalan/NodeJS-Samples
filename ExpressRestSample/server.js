c6f8540b04d58506598e873d91b30be53f865fb3
SanthanaRajagopalan <santhana_rajagopalan@hotmail.com>
2014-08-21 22:02:46 -0400

    Initial Commit

---
+++ ExpressRestSample/server.js
@@ -0,0 +1,90 @@
+/**
+* Description: Sample Rest Service. 
+* Date: 7/16/2014
+*/
+var express = require('express'),
+    fs = require('fs'),
+    dns = require('dns'),
+    hostName = require('os').hostname(),
+    htmlContent,
+	delay = 100;
+
+var app = express();
+
+app.set('views', __dirname + '/views/template');
+app.set('view engine', 'jade');
+
+// Enables CORS
+var enableCORS = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+ 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
+ 
+// enable CORS!
+app.use(enableCORS);
+
+function response(req, res) {
+	var fileName,
+		resourceName;
+	if (req.params.name) {
+		resourceName = req.params.name;
+		if (resourceName.indexOf(".html") > 0) {
+			fileName = "./response/" + resourceName;
+		} else if (resourceName.indexOf(".json") > 0) {
+			fileName = "./response/" + resourceName;
+		} else if (resourceName.indexOf(".png") > 0) {
+			fileName = "./response" + resourceName;
+		} else if (resourceName.indexOf(".xml") > 0) {
+			fileName = "./response" + resourceName;
+		} else if (resourceName.indexOf(".jpeg") > 0) {
+			fileName = "./response" + resourceName;
+		}
+	}
+	fs.readFile(fileName, 'utf8', function (err, html) {
+		if (err) {
+			throw err;
+		}
+		console.log("Sending Response", new Date());
+		htmlContent = html.toString();
+
+		setTimeout(function () {
+			res.header("200");
+			res.header('Content-Type:text/html');
+			res.header('Content-Length:' + htmlContent.length);
+			res.send(htmlContent);
+        }, delay);
+	});
+
+}
+
+app.get('/', function(req, res){
+	res.status(200);
+	res.render('index');
+});
+
+app.get('/404', function(req, res){
+	res.status(200);
+	res.render('index');
+});
+
+app.get('/getResponse/:name', response);
+
+// 404 route
+app.get('*', function(req, res){
+res.status(404);
+res.render('404', { url: req.url });
+});
+
+dns.lookup(hostName, function (err, addr, fam) {
+	var server = app.listen(9090, addr, function () {
+		console.log('App is running at URL http://%s:%s', server.address().address, server.address().port);
+	});
+});
